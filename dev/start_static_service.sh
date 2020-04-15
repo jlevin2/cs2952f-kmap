7e289ad5e18247a97a64934c5e5abb4fc84eb65a
Josh Levin <joshua_levin2@brown.edu>
2020-04-15 17:19:56 -0400

    report updates

--- dev/start_static_service.sh
+++ dev/start_static_service.sh
@@ -4,7 +4,7 @@ export SERVICE_NAME=1
 #--log-level debug
 #LD_PRELOAD=/code/envoy.so /usr/local/bin/envoy -c /etc/service-envoy.yaml --service-cluster service${SERVICE_NAME} --log-level debug > /dev/null 2>&1  &
 #LD_PRELOAD=/code/service.so python3 /code/service.py &
-/usr/local/bin/envoy -c /etc/service-envoy.yaml --service-cluster service${SERVICE_NAME} --log-level error &
+/usr/local/bin/envoy -c /etc/tinyserver-envoy.yaml --service-cluster service${SERVICE_NAME} --log-level error &
 gcc -g -o tiny tiny.c
 ./tiny 8080
 
